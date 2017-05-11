5d1fd17dfbc83f8a43dcb6b45a7905a920af3cc6
jismy <jismy@toobler.com>
2017-05-11 11:40:50 +0530

    final

--- routes/expression.js
+++ routes/expression.js
@@ -3,19 +3,52 @@ var router = express.Router();
 var fs = require('fs');
 var cheerio = require('cheerio')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  
-var cont="<section class='aboutus'>asd123</section>";
-var contents = fs.readFileSync('./input.txt', 'utf8');
-$ = cheerio.load(contents);
- var sections=[];
-var h2 = $('section.aboutus').each(function(i,elem){
- sections[i] = $(elem).html();
- //console.log($(elem).find('div').text());
-});
 
-res.send(sections);
+router.get('/', function(req, res, next) {
+
+    var sections = ["aboutus", "test", "ggg"];
+    var classes = ["wbt", "wbi", "www"];
+    var contents = fs.readFileSync('./input.txt', 'utf8');
+    var new_contents = contents;
+    var extract = [];
+    for (var i = 0; i < sections.length; i++) {
+
+        $ = cheerio.load(new_contents);
+        $('section.' + sections[i]).each(function(j, elem) {
+            var sec_extract = $(elem).html();
+
+            $ = cheerio.load(sec_extract);
+
+            for (var l = 0; l < classes.length; l++) {
+                $("." + classes[l]).each(function(k, el) {
+                    var cls_extract = $(el).html();
+
+                    var obj = {
+                        "section": sections[i],
+                        "class": classes[l]
+                    };
+                    extract.push(obj);
+                });
+            }
+        });
+        $ = cheerio.load(new_contents);
+        $('section.' + sections[i]).replaceWith('');
+        new_contents = $.html();
+    }
+    $ = cheerio.load(new_contents);
+    for (var l = 0; l < classes.length; l++) {
+        $("." + classes[l]).each(function(k, el) {
+            var cls_extract = $(el).html();
+
+            var obj = {
+                "section": "",
+                "class": classes[l]
+            };
+            extract.push(obj);
+        });
+    }
+
+    res.send(extract);
 });
 
 module.exports = router;
